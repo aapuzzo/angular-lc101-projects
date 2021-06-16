34e09ee236c249241fe055be7d33a0127c986b48
ACA <anthony_apuzzo@comcast.com>
2021-06-16 15:42:23 -0500

    buttons added

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,20 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew(candidate: object) {
+    if (!this.crew.includes(candidate)){
+      this.crew.push(candidate);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
+  changeMissionName(name: string) {
+    if (name === " ") {
+      this.missionName = "LaunchCode Moonshot"
+    }    
+    this.missionName = name;
+    return false;
+  }
 
 
 }
