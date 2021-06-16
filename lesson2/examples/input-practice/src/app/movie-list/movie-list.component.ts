5f42376e9ec7ab5a551c83c78da548e817714da4
ACA <anthony_apuzzo@comcast.com>
2021-06-16 14:29:31 -0500

    bonuses complete

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,21 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+   //declare a variable then use ngIf
+   addMovie (newTitle: string) {
+      let errorMsg = '';
+      if(newTitle === ''){
+         errorMsg = 'Please enter a movie title.';
+      }
+       else if (this.movies.includes(newTitle)) {
+         errorMsg = `${newTitle} is already in your movie list.`;
+      }      
+      else if (newTitle === "secret") {
+         errorMsg = "You found the secret";
+      }
+       else {
+         this.movies.push(newTitle);
+      }
+      return errorMsg;
+   }
 }
